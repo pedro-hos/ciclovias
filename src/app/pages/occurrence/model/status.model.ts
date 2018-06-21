7a8d83fae71d2890d45c916c7c3996e34b42607e
pedro-hos <pedro-hos@outlook.com>
2018-06-21 19:51:22 -0300

    tentando deploy

--- src/app/pages/occurrence/model/status.model.ts
+++
@@ -1,5 +0,0 @@
-export class Status {
-  id: number;
-  name: string;
-  description: string;
-};
--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  resouceUrl: 'http://ciclovia-server-ciclovia-server.193b.starter-ca-central-1.openshiftapps.com/api',
-  production: true
-};
